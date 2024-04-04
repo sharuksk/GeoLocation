4fcfb35d843fdcacf125b7cb22d672f70e71837f
sharuksk <sharukajmal2@gmail.com>
2024-04-04 11:33:53 +0300

    video file process

--- src/COMPONENT/dashBoard.js
+++ src/COMPONENT/dashBoard.js
@@ -1,55 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { Box, ThemeProvider } from '@mui/material';
 import DropFileInput from './components/drop-file-input/DropFileInput';
 import UploadButton from './components/upload-button/UploadButton';
-import { useState } from 'react';
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage"
-import { storage, db } from './firebase';
-import { doc, setDoc } from "firebase/firestore"
 //import '../SCSS/dashBoard.scss'
-
 import './dashBoard.css'
+import {linkNode} from "../nodelink";
+import axios from "axios";
+import FileBase64 from "react-file-base64";
 
 export default function DashBoard () {
-  const [file, setFile] = useState(null)
+  const [base, setBase] = useState("");
+  const [docTitle, setDocTitle] = useState("");
+  const [state, setState] = useState(false);
+  const [video, setVideo] = useState("");
 
-    const onFileChange = (files) => {
-        const currentFile = files[0]
-        setFile(currentFile)
-        console.log(files);
+  useEffect(() =>{
+    //handleGetVideo();
+    console.log(state);
+  }, [state]);
+
+
+    const handleGetVideo = async() => {
+        await axios.post(`${linkNode}/getvideo`).then(async (res) => {
+            console.log(res.data.message);
+        });
     }
+    const handleClick = async() => {
+        try {
 
-    const uploadToDatabase = (url) => {
-        let docData = {
-            mostRecentUploadURL: url,
-            username: "jasondubon"
+            let Obj = {
+                fileName: docTitle,
+                file64: base.toString(),
+            };
+
+            await axios.post(`${linkNode}/postvideo`, Obj).then(async (res) => {
+                console.log(res.data.message);
+            });
+            
+        } catch (err) {
+          console.log(err);
         }
-        const userRef = doc(db, "users", docData.username)
-        setDoc(userRef, docData, {merge: true}).then(() => {
-            console.log("successfully updated DB")
-        }).catch((error) => {
-            console.log("errrror")
-        })
-    }
+      };
 
-    const handleClick = () => {
-        if (file === null) return;
-        const fileRef = ref(storage, `videos/${file.name}`)
-        const uploadTask = uploadBytesResumable(fileRef, file)
-
-        uploadTask.on('state_changed', (snapshot) => {
-            let progress = (snapshot.bytesTransferred/ snapshot.totalBytes) * 100
-            console.log(progress)
-        }, (error) => {
-            console.log("error :(")
-        }, () => {
-            console.log("success!!")
-            getDownloadURL(uploadTask.snapshot.ref).then(downloadURL =>{
-                uploadToDatabase(downloadURL)
-                console.log(downloadURL)
-            })
-        })
-    }
+    
   return (
     <>
         <table>
@@ -59,17 +52,27 @@ export default function DashBoard () {
                         <h2 className="header">
                             React drop files input
                         </h2>
-                        <DropFileInput
-                            onFileChange={(files) => onFileChange(files)}
+                        {/* <DropFileInput
+                            onFileChange={(e) => {
+                                setBase(e.base64)
+                                setDocTitle(e.name)
+                                }}
                         />
-                        <br></br>
-                        <UploadButton onClick={ () => handleClick()}> </UploadButton>
+                        <br></br> */}
+                        <FileBase64
+                            onDone={(e) => {
+                              setDocTitle(e.name);
+                              setBase(e.base64);
+                            }}/>
+                            <br></br>
+                        <UploadButton onClick={(e) => handleClick()}/>
                     </div>
                 </td>
                 <td>
                     <div className="box1">
-                    <video width="750" height="500" controls>
-                        <source src="https://firebasestorage.googleapis.com/v0/b/geolocation-53bce.appspot.com/o/videos%2FWhatsApp%20Video%202024-03-25%20at%205.24.15%20PM.mp4?alt=media&token=7622d6ca-97e9-4d24-9399-4ef5f6313b7c" />
+                    <video className="video" width="750" height="500" controls>
+                        <source src ={video} />
+                        {/* <source src="https://firebasestorage.googleapis.com/v0/b/geolocation-53bce.appspot.com/o/videos%2FWhatsApp%20Video%202024-03-25%20at%205.24.15%20PM.mp4?alt=media&token=7622d6ca-97e9-4d24-9399-4ef5f6313b7c" /> */}
                     </video>
                     </div> 
                 </td>
---
+++ src/nodelink.js
@@ -0,0 +1,2 @@
+export const linkNode = "http://localhost:5000/dashboard";
+// export const linkNode = "https://wasenderbe.onrender.com/ablelyfwas"
