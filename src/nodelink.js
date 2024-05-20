47520f0e517439a9209e9ebfd1984bcd29dc0151
sharuksk <sharukajmal2@gmail.com>
2024-05-20 17:09:27 +0300

    deploy netlify

--- src/nodelink.js
+++ src/nodelink.js
@@ -1,2 +1,2 @@
-export const linkNode = "http://localhost:5000/dashboard";
+export const linkNode = "https://geolocation-be.onrender.com/";
 // export const linkNode = "https://wasenderbe.onrender.com/ablelyfwas"
